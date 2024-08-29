6bee6f0d5ae10036a40b9a4076b26bb16707075e
Moksleivis <Moksleivis@kitm.local>
2024-08-29 12:57:26 +0300

    egzaminas

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,192 @@
+<x-app-layout>
+    
+
+   
+
+                <!--
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/aspect-ratio'),
+    ],
+  }
+  ```
+-->
+
+
+
+<div class="w-100 mx-auto bg-danger mt-5 rounded-lg" style="width:80%;">
+    <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+      <h2 class="sr-only text-center text-2xl mb-40">PRODUKTAI</h2>
+
+      
+            <div href="#" class="grid grid-cols-2 gap-x-2 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+                
+                
+                <div class="group">
+                <div class="skate1 aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg  xl:aspect-h-8 xl:aspect-w-7 ">
+                    <img src="https://www.skatehut.co.uk/media/catalog/product/E/N/ENU_320_SKU_MIN_PIN_725_0.jpg?fit=bounds&quality=80&width=400&height=400" alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." class="h-full w-full object-cover object-center group-hover:opacity-100 sm:rounded-lg" style="width:70%; margin-left: auto;
+        margin-right: auto;">
+                </div>
+                <h3 class="mt-4 text-sm text-gray-700 text-center">Skateboard 1</h3>
+                <p class="mt-1 text-lg font-medium text-gray-900 text-center">$989</p>
+                <div class="text-center">
+                <a href="/skateboards/1">Placiau</a>
+                </div>
+
+                
+                </div>
+
+            <div href="#" class="group">
+            <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden  xl:aspect-h-8 xl:aspect-w-7">
+                <img src="https://www.skatehut.co.uk/media/catalog/product/S/A/SAN_1023_SLG_COM_8.jpg?fit=bounds&quality=80&width=400&height=400" alt="Person using a pen to cross a task off a productivity paper card." class="h-full w-full object-cover object-center group-hover:opacity-75 sm:rounded-lg" style="width:70%; margin-left: auto;
+        margin-right: auto;">
+            </div>
+            <h3 class="mt-4 text-sm text-gray-700 text-center">Skateboard 2</h3>
+            <p class="mt-1 text-lg font-medium text-gray-900 text-center">$598</p>
+            <div class="text-center">
+                <a href="/skateboards/1">Placiau</a>
+                </div>
+            </div>
+
+            <div href="#" class="group">
+                <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg  xl:aspect-h-8 xl:aspect-w-7">
+                <img src="https://www.skatehut.co.uk/media/catalog/product/E/N/ENU_614_GRA_II_MIN_PIN_0.jpg?fit=bounds&quality=80&width=400&height=400" alt="Olive drab green insulated bottle with flared screw lid and flat top." class="h-full w-full object-cover object-center group-hover:opacity-75 sm:rounded-lg" style="width:70%; margin-left: auto;
+        margin-right: auto;">
+                </div>
+                <h3 class="mt-4 text-sm text-gray-700 text-center">Skateboard 3</h3>
+                <p class="mt-1 text-lg font-medium text-gray-900 text-center">$555</p>
+                <div class="text-center">
+                <a href="/skateboards/1">Placiau</a>
+                </div>
+            </div>
+
+            <div href="#" class="group">
+            <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg  xl:aspect-h-8 xl:aspect-w-7">
+                <img src="https://www.skatehut.co.uk/media/catalog/product/E/N/ENU_POW_BLU_0_4152.jpg?fit=bounds&quality=80&width=400&height=400" alt="Hand holding black machined steel mechanical pencil with brass tip and top." class="h-full w-full object-cover object-center group-hover:opacity-75 sm:rounded-lg" style="width:70%; margin-left: auto;
+        margin-right: auto;">
+            </div>
+            <h3 class="mt-4 text-sm text-gray-700 text-center">Skateboard 4</h3>
+            <p class="mt-1 text-lg font-medium text-gray-900 text-center">$555</p>
+            <div class="text-center">
+                <a href="/skateboards/1">Placiau</a>
+                </div>
+            
+            </div>
+
+            <div href="#" class="group ">
+                <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden  xl:aspect-h-8 xl:aspect-w-7 ">
+                <img src="https://www.skatehut.co.uk/media/catalog/product/B/I/BIR_221_ST3_HAW_SK2_COM_775_20.jpg?fit=bounds&quality=80&width=400&height=400" alt="Hand holding black machined steel mechanical pencil with brass tip and top." class="h-full w-full object-cover object-center group-hover:opacity-75 sm:rounded-lg" style="width:70%; margin-left: auto;
+        margin-right: auto;">
+                </div>
+                <h3 class="mt-4 text-sm text-gray-700 text-center">Skateboard 5</h3>
+                <p class="mt-1 text-lg font-medium text-gray-900 text-center">$875</p>
+                <div class="text-center">
+                <a href="/skateboards/1">Placiau</a>
+                </div>
+                
+            </div>
+            
+            <div href="#" class="group">
+                <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg  xl:aspect-h-8 xl:aspect-w-7 ">
+                <img src="https://plaze-shop.de/wp-content/uploads/2022/05/Santa_Cruz_Braun_Munchies_Everslick_8.25_plaze-shop_1.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." class="h-full w-full object-cover object-center group-hover:opacity-75 sm:rounded-lg" style="width:70%; margin-left: auto;
+        margin-right: auto;">
+                </div>
+                <h3 class="mt-4 text-sm text-gray-700 text-center">Skateboard 6</h3>
+                <p class="mt-1 text-lg font-medium text-gray-900 text-center">$452</p>
+                <div class="text-center">
+                <a href="/skateboards/1">Placiau</a>
+                </div>
+                
+            </div>
+
+            <div href="#" class="group">
+                <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg  xl:aspect-h-8 xl:aspect-w-7">
+                <img src="https://plaze-shop.de/wp-content/uploads/2022/05/Santa_Cruz_Braun_Munchies_Everslick_8.25_plaze-shop_1.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." class="h-full w-full object-cover object-center group-hover:opacity-75 sm:rounded-lg" style="width:70%; margin-left: auto;
+        margin-right: auto;">
+                </div>
+                <h3 class="mt-4 text-sm text-gray-700 text-center">Skateboard 7</h3>
+                <p class="mt-1 text-lg font-medium text-gray-900 text-center">$956</p>
+                <div class="text-center">
+                <a href="/skateboards/1">Placiau</a>
+                </div>
+
+            </div>
+            <div href="#" class="group">
+                <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg -200 xl:aspect-h-8 xl:aspect-w-7">
+                <img src="https://plaze-shop.de/wp-content/uploads/2022/05/Santa_Cruz_Braun_Munchies_Everslick_8.25_plaze-shop_1.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." class="h-full w-full object-cover object-center group-hover:opacity-75 sm:rounded-lg" style="width:70%; margin-left: auto;
+        margin-right: auto;">
+                </div>
+                <h3 class="mt-4 text-sm text-gray-700 text-center">Skateboard 8</h3>
+                <p class="mt-1 text-lg font-medium text-gray-900 text-center">$364</p>
+                <div class="text-center">
+                <a href="/skateboards/1">Placiau</a>
+                </div>
+
+            </div>
+            <div href="#" class="group">
+                <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg  xl:aspect-h-8 xl:aspect-w-7 ">
+                <img src="https://plaze-shop.de/wp-content/uploads/2022/05/Santa_Cruz_Braun_Munchies_Everslick_8.25_plaze-shop_1.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." class="h-full w-full object-cover object-center group-hover:opacity-75 sm:rounded-lg" style="width:70%; margin-left: auto;
+        margin-right: auto;">
+                </div>
+                <h3 class="mt-4 text-sm text-gray-700 text-center">Skateboard 8</h3>
+                <p class="mt-1 text-lg font-medium text-gray-900 text-center">$325</p>
+                <div class="text-center">
+                <a href="/skateboards/1">Placiau</a>
+                </div>
+
+            </div>
+
+            <div href="#" class="group">
+                <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg  xl:aspect-h-8 xl:aspect-w-7 ">
+                <img src="https://plaze-shop.de/wp-content/uploads/2022/05/Santa_Cruz_Braun_Munchies_Everslick_8.25_plaze-shop_1.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." class="h-full w-full object-cover object-center group-hover:opacity-75 sm:rounded-lg" style="width:70%; margin-left: auto;
+        margin-right: auto;">
+                </div>
+                <h3 class="mt-4 text-sm text-gray-700 text-center">Skateboard 9</h3>
+                <p class="mt-1 text-lg font-medium text-gray-900 text-center">$740</p>
+                <div class="text-center">
+                <a href="/skateboards/1">Placiau</a>
+                </div>
+
+            </div>
+            <div href="#" class="group">
+                <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg  xl:aspect-h-8 xl:aspect-w-7 ">
+                <img src="https://www.sickboards.nl/95151-large_default/enjoi-dark-side-of-premium-black-90-skateboard-complete.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." class="h-full w-full object-cover object-center group-hover:opacity-75 sm:rounded-lg" style="width:70%; margin-left: auto;
+        margin-right: auto;">
+                </div>
+                <h3 class="mt-4 text-sm text-gray-700 text-center">Skateboard 9</h3>
+                <p class="mt-1 text-lg font-medium text-gray-900 text-center">$740</p>
+                <div class="text-center">
+                <a href="/skateboards/1">Placiau</a>
+                </div>
+
+            </div>
+            <div href="#" class="group">
+                <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg  xl:aspect-h-8 xl:aspect-w-7">
+                <img src="https://www.sickboards.nl/96818-large_default/almost-ren-stimpy-on-my-back-w-soft-wheels-green-70-skateboard-complete.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." class="h-full w-full object-cover object-center group-hover:opacity-75 sm:rounded-lg" style="width:70%; margin-left: auto;
+        margin-right: auto;">
+                </div>
+                <h3 class="mt-4 text-sm text-gray-700 text-center">Skateboard 9</h3>
+                <p class="mt-1 text-lg font-medium text-gray-900 text-center">$740</p>
+                <div class="text-center">
+                <a href="/skateboards/1">Placiau</a>
+                </div>
+
+            </div>
+
+
+        <!-- More products... -->
+      </div>
+    </div>
+  </div>
+
+
+            </div>
+        </div>
+    </div>
+</x-app-layout>
---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            colors: {
+                    transparent: 'transparent',
+                    current: 'currentColor',
+                    'white': '#ffffff',
+                    'purple': '#3f3cbb',
+                    'midnight': '#121063',
+                    'metal': '#565584',
+                    'tahiti': '#3ab7bf',
+                    'silver': '#ecebff',
+                    'bubble-gum': '#ff77e9',
+                    'bermuda': '#78dcca',
+              },
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+        
+    },
+
+    plugins: [forms, typography],
+};
